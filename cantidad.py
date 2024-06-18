b0cb38c1eac537395a19377259c226141514eecd
TataCthullu <gonzalo.org@gmail.com>
2024-06-18 16:53:08 -0300

    terminado 1

--- cantidad.py
+++ cantidad.py
@@ -1,9 +1,8 @@
-cantidad_cigarros_al_dia = int(input("Escribe la cantidad de cigarros al dia: "))
+cantidad_cigarros_al_dia = 20
 print(f"Fumas {cantidad_cigarros_al_dia} cigarros al dia")
-
+#funcion calcular cantidad de cigarros
 def cigarros_al_mes():
     cantidad_cigarros_mes = cantidad_cigarros_al_dia * 30
     return cantidad_cigarros_mes
 
 print(f"Fumas {cigarros_al_mes()} cigarros al mes")
-#hooooooo
---
+++ valor.py
@@ -0,0 +1,7 @@
+from cantidad import cigarros_al_mes
+
+
+valor_unidad = 150
+valor_al_mes = cigarros_al_mes() * valor_unidad
+
+print(f"Gastas {valor_al_mes} al mes")

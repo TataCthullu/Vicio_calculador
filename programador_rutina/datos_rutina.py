9a6d967b2968f1ef8b9e89e4302f0c01e79a62ff
TataCthullu <gonzalo.org@gmail.com>
2024-06-19 03:41:53 -0300

    terminado 2c

--- programador_rutina/datos_rutina.py
+++ programador_rutina/datos_rutina.py
@@ -7,12 +7,35 @@ actions = {
 }
 
 pers_cont = 0
-inp_act = input("Ingresa la actividad que realizaste: ")
 
-if inp_act in actions:
-    pers_cont += actions[inp_act]
-    print(pers_cont)
-else: 
-    print("La actividad no se encuentra en el diccionario")    
+def Menu():
+    print("1. Actividad realizada")
+    print("2. Mostrar Contador")
+    print("3. Salir")
 
+def Act_rea():
+    global pers_cont
+    inp_act = input("Ingresa la actividad que realizaste: ")
 
+    if inp_act in actions:
+        pers_cont += actions[inp_act]
+        print(f"Puntos acumulados: {pers_cont}")
+    else: 
+        print("La actividad no se encuentra en el diccionario")    
+
+def Most_cont():
+    print(f"Puntos obtenidos: {pers_cont}")
+
+while True:
+    Menu()
+    opcion = input("Selecciona una opcion: ")    
+
+    if opcion == "1":
+        Act_rea()
+    elif opcion == "2":
+        Most_cont()
+    elif opcion == "3":
+        print("Cerrando el programa...")
+        break        
+    else:
+        print("Opción no válida, elige una q lo sea.")
